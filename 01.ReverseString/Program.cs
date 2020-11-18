eded64491ca02d83cd6b4680efce636313364a70
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-18 20:17:24 +0200

    Add project files.

---
+++ 01.ReverseString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string result1 = string.Empty;
+
+                for (int i = input.Length - 1; i >= 0 ; i--)
+                {
+                    result1 += input[i];
+                }
+                result.Add(input, result1);
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+        }
+    }
+}
---
+++ StringManipulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string result = string.Empty;
+
+            foreach (var item in arr)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    result += item;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
