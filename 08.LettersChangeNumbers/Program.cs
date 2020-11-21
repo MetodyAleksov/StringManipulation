e0fd655a084980bd7c9eddb0989498d0afc0d60a
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-21 16:19:54 +0200

    updated

--- 08.LettersChangeNumbers/Program.cs
+++ 08.LettersChangeNumbers/Program.cs
@@ -6,7 +6,46 @@ namespace _08.LettersChangeNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            double totalsum = 0;
+
+            foreach (var item in input)
+            {
+                char letter1 = item[0];
+                char letter2 = item[item.Length - 1];
+                string number = String.Empty;
+                foreach (var ch in item)
+                {
+                    if (Char.IsDigit(ch))
+                    {
+                        number += ch;
+                    }
+                }
+                double n = double.Parse(number);
+
+                if (Char.IsUpper(letter1))
+                {
+                    n /= (int)(letter1 - 64);
+                }
+                else
+                {
+                    n *= (int)(letter1 - 96);
+                }
+
+                if (Char.IsUpper(letter2))
+                {
+                    n -= (int)(letter2 - 64);
+                }
+                else
+                {
+                    n += (int)(letter2 - 96);
+                }
+
+                totalsum += n;
+            }
+            Console.WriteLine($"{totalsum:f2}");
         }
     }
 }
