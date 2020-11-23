c69b92a00b53b2cb1b205f5e6af7373f520527b8
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-11-23 19:21:44 +0200

    updated

---
+++ 01.ExtractPersonalInformation/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ExtractPersonalInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> persons = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int startName = 0;
+                int endName = 0;
+                int startAge = 0;
+                int endAge = 0;
+
+                for (int k = 0; k < input.Length; k++)
+                {
+                    if (input[k] == '@')
+                    {
+                        startName = k;
+                    }
+                    if (input[k] == '|')
+                    {
+                        endName = k;
+                    }
+                    if (input[k] == '#')
+                    {
+                        startAge = k;
+                    }
+                    if (input[k] == '*')
+                    {
+                        endAge = k;
+                    }
+                }
+                int lenghtName = endName - startName - 1;
+                string name = input.Substring(startName + 1, lenghtName);
+                int ageLenght = endAge - startAge - 1;
+                string age = input.Substring(startAge + 1, ageLenght);
+                persons.Add(name, age);
+            }
+
+            foreach (var item in persons)
+            {
+                Console.WriteLine($"{item.Key} is {item.Value} years old.");
+            }
+        }
+    }
+}
---
+++ 02.AciiSumator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02.AciiSumator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number1 = Console.ReadLine();
+            int n1 = (int)number1[0];
+            string number2 = Console.ReadLine();
+            int n2 = (int)number2[0];
+            int longerNum = 0;
+            int shorterNum = 0;
+            if (n1 >= n2)
+            {
+                longerNum = n1;
+                shorterNum = n2;
+            }
+            else
+            {
+                longerNum = n2;
+                shorterNum = n1;
+            }
+            int sum = 0;
+
+            string input = Console.ReadLine();
+
+            foreach (var item in input)
+            {
+                if (item < longerNum && item > shorterNum)
+                {
+                    sum += (int)item;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
